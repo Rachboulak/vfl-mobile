b2f74e6c533a36d7942681b19a11e9dd5d0a936a
Rachboulak <r.boulakhzine@gmail.com>
2016-08-04 01:46:46 +0100

    init POC VFL Mobile

---
+++ app/app.ts
@@ -0,0 +1,43 @@
+import {Component} from '@angular/core';
+import {Platform, ionicBootstrap} from 'ionic-angular';
+import {StatusBar} from 'ionic-native';
+import {HomePage} from './pages/home/home';
+
+import {provide} from '@angular/core';
+import {Http, HTTP_PROVIDERS} from '@angular/http';
+import {TranslateService, TranslatePipe, TranslateLoader, TranslateStaticLoader} from 'ng2-translate/ng2-translate';
+
+@Component({
+  template: '<ion-nav [root]="rootPage"></ion-nav>'
+})
+export class MyApp {
+  rootPage: any = HomePage;
+
+  constructor(platform: Platform, private translate: TranslateService) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+      //i18n
+      this.translateConfig();
+    });
+  }
+
+  translateConfig() {
+    var userLang = navigator.language.split('-')[0]; // use navigator lang if available
+    userLang = /(en|fr)/gi.test(userLang) ? userLang : 'en';
+ 
+    // this language will be used as a fallback when a translation isn't found in the current language
+    this.translate.setDefaultLang('en');
+ 
+    // the lang to use, if the lang isn't available, it will use the current loader to get them
+    this.translate.use(userLang);
+  }
+}
+
+ionicBootstrap(MyApp, [[provide(TranslateLoader, {
+  useFactory: (http: Http) => new TranslateStaticLoader(http, 'assets/i18n', '.json'),
+  deps: [Http]
+}),
+  TranslateService]], {
+});
---
+++ app/pages/home/home.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {TranslatePipe} from "ng2-translate/ng2-translate";
+ 
+@Component({
+  templateUrl: 'build/pages/home/home.html',
+  pipes: [TranslatePipe]
+})
+export class HomePage {
+  constructor(private navCtrl: NavController) {
+
+  }
+}
